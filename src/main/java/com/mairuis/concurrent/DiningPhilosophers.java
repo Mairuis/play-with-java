0652bd079337a8c266c0fe907bff49bcfdceecab
Maries <xh001x@hotmail.com>
2020-05-03 20:50:54 +0800

    Initial commit

---
+++ src/main/java/com/mairuis/concurrent/DiningPhilosophers.java
@@ -0,0 +1,66 @@
+package com.mairuis.concurrent;
+
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+/**
+ * 哲学家就餐问题
+ * 5个人，5个叉子
+ *
+ * @author Mairuis
+ * @since 2020/5/3
+ */
+public abstract class DiningPhilosophers {
+
+    public abstract void eat(int philosophers);
+
+    public abstract int count();
+
+    /**
+     * 尝试拿起第二把叉子，如果失败则全部释放的解法
+     */
+    public static class CASImplementSolution extends DiningPhilosophers {
+        private final AtomicBoolean[] forks = IntStream.range(0, 6).mapToObj(x -> new AtomicBoolean(false)).toArray(AtomicBoolean[]::new);
+        private final AtomicInteger eatCount = new AtomicInteger(0);
+
+        @Override
+        public void eat(int philosophers) {
+            AtomicBoolean right = forks[philosophers];
+            AtomicBoolean left = forks[philosophers - 1 < 0 ? forks.length - 1 : philosophers - 1];
+            for (; ; ) {
+                if (!right.compareAndSet(false, true)) {
+                    continue;
+                }
+                if (!left.compareAndSet(false, true)) {
+                    right.compareAndSet(true, false);
+                    continue;
+                }
+                left.compareAndSet(true, false);
+                right.compareAndSet(true, false);
+                break;
+            }
+            eatCount.incrementAndGet();
+        }
+
+        @Override
+        public int count() {
+            return eatCount.get();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DiningPhilosophers diningPhilosophers = new CASImplementSolution();
+        long time = System.currentTimeMillis();
+        Threads.start(100, (i) -> {
+            int N = 100000;
+            while (N > 0) {
+                diningPhilosophers.eat(i % 5);
+                N -= 1;
+            }
+        }).join();
+        long passTime = System.currentTimeMillis() - time;
+        System.out.println("success " + diningPhilosophers.count() + " 耗时 " + passTime + "ms");
+    }
+}
---
+++ src/main/java/com/mairuis/concurrent/Threads.java
@@ -0,0 +1,34 @@
+package com.mairuis.concurrent;
+
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+
+/**
+ * @author Mairuis
+ * @since 2020/5/3
+ */
+public class Threads {
+
+    private final Thread[] threads;
+
+    private Threads(int n, IntConsumer runnable) {
+        this.threads = new Thread[n];
+        for (int i = 0; i < threads.length; i += 1) {
+            int finalI = i;
+            this.threads[i] = new Thread(() -> {
+                runnable.accept(finalI);
+            }, "T-" + i);
+            this.threads[i].start();
+        }
+    }
+
+    public void join() throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    public static Threads start(int n, IntConsumer runnable) {
+        return new Threads(n, runnable);
+    }
+}
