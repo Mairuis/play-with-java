72db499ee327d6728f93d779069e270616d4fa05
Maries <xh001x@hotmail.com>
2021-06-10 00:21:49 +0800

    测试Unsafe读取数据速度

---
+++ src/main/java/com/mairuis/jvm/util/Utils.java
@@ -0,0 +1,40 @@
+package com.mairuis.jvm.util;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
+
+/**
+ * @author Mairuis
+ * @since 2021/6/9
+ */
+public class Utils {
+
+    private static final Unsafe THE_UNSAFE;
+
+    static {
+        try {
+            final PrivilegedExceptionAction<Unsafe> action = () -> {
+                Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+                theUnsafe.setAccessible(true);
+                return (Unsafe) theUnsafe.get(null);
+            };
+
+            THE_UNSAFE = AccessController.doPrivileged(action);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load unsafe", e);
+        }
+    }
+
+    /**
+     * Get a handle on the Unsafe instance, used for accessing low-level concurrency
+     * and memory constructs.
+     *
+     * @return The Unsafe
+     */
+    public static Unsafe getUnsafe() {
+        return THE_UNSAFE;
+    }
+}
